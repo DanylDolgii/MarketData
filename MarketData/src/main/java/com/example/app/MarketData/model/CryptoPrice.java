b243a8d3b2d9d9285796620d7cf343a80d2b4d10
DanylDolgii <danyldolgii777@gmail.com>
2024-04-11 18:03:53 +0300

    Define entity classes

--- MarketData/src/main/java/com/example/app/MarketData/model/CryptoPrice.java
+++ MarketData/src/main/java/com/example/app/MarketData/model/CryptoPrice.java
@@ -1,4 +1,19 @@
 package com.example.app.MarketData.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import org.springframework.data.annotation.Id;
+
+@Entity
 public class CryptoPrice {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String currencyPair;
+
+    private Double averagePrice;
+
+    // getters and setters
 }
