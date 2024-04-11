c670bcd0362154e98637e7ad7594d4d15131de66
DanylDolgii <danyldolgii777@gmail.com>
2024-04-11 18:22:24 +0300

    Add constructor, getters and setters; modify ID annotation

--- MarketData/src/main/java/com/example/app/MarketData/model/CryptoPrice.java
+++ MarketData/src/main/java/com/example/app/MarketData/model/CryptoPrice.java
@@ -3,17 +3,42 @@ package com.example.app.MarketData.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import org.springframework.data.annotation.Id;
+import jakarta.persistence.Id;
 
 @Entity
 public class CryptoPrice {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String currencyPair;
-
     private Double averagePrice;
 
+    // Constructor
+    public CryptoPrice() {
+    }
+
     // getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCurrencyPair() {
+        return currencyPair;
+    }
+
+    public void setCurrencyPair(String currencyPair) {
+        this.currencyPair = currencyPair;
+    }
+
+    public Double getAveragePrice() {
+        return averagePrice;
+    }
+
+    public void setAveragePrice(Double averagePrice) {
+        this.averagePrice = averagePrice;
+    }
 }
