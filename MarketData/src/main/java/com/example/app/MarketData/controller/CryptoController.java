9422f11c4e0ad098f46063aaeffd2541008726ad
DanylDolgii <danyldolgii777@gmail.com>
2024-04-12 16:32:24 +0300

    Fixed the code

--- MarketData/src/main/java/com/example/app/MarketData/controller/CryptoController.java
+++ MarketData/src/main/java/com/example/app/MarketData/controller/CryptoController.java
@@ -3,7 +3,6 @@ package com.example.app.MarketData.controller;
 
 import com.example.app.MarketData.model.CryptoPrice;
 import com.example.app.MarketData.service.CryptoService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,16 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/crypto")
+@RequestMapping("/api/crypto")
 public class CryptoController {
     private final CryptoService cryptoService;
 
-    @Autowired
     public CryptoController(CryptoService cryptoService) {
         this.cryptoService = cryptoService;
     }
 
-    @GetMapping("/arerage")
+    @GetMapping("/average")
     public List<CryptoPrice> getAverageCryptoPrices() {
         return cryptoService.getAverageCryptoPrices();
     }
