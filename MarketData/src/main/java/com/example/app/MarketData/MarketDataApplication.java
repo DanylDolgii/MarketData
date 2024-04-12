2000f07ce867bee07528cc02ca97d246a386d8c8
DanylDolgii <danyldolgii777@gmail.com>
2024-04-12 12:16:38 +0300

    Add @EnableJpaRepositories

--- MarketData/src/main/java/com/example/app/MarketData/MarketDataApplication.java
+++ MarketData/src/main/java/com/example/app/MarketData/MarketDataApplication.java
@@ -2,12 +2,13 @@ package com.example.app.MarketData;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class MarketDataApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MarketDataApplication.class, args);
 	}
-
 }
