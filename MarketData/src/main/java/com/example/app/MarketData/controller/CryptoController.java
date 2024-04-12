d62498bcb83b8933e94cb35e75cc5c55c091c125
DanylDolgii <danyldolgii777@gmail.com>
2024-04-12 12:35:58 +0300

    Modify a controller class to handle HTTP requests

--- MarketData/src/main/java/com/example/app/MarketData/controller/CryptoController.java
+++ MarketData/src/main/java/com/example/app/MarketData/controller/CryptoController.java
@@ -1,4 +1,27 @@
 package com.example.app.MarketData.controller;
 
+
+import com.example.app.MarketData.model.CryptoPrice;
+import com.example.app.MarketData.service.CryptoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/crypto")
 public class CryptoController {
+    private final CryptoService cryptoService;
+
+    @Autowired
+    public CryptoController(CryptoService cryptoService) {
+        this.cryptoService = cryptoService;
+    }
+
+    @GetMapping("/arerage")
+    public List<CryptoPrice> getAverageCryptoPrices() {
+        return cryptoService.getAverageCryptoPrices();
+    }
 }
