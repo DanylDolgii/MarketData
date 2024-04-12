939fe2fbb091ea8497f30ba1ac53ba17fdbf5664
DanylDolgii <danyldolgii777@gmail.com>
2024-04-12 18:00:31 +0300

    Hid the code

--- MarketData/src/main/java/com/example/app/MarketData/controller/CryptoController.java
+++ MarketData/src/main/java/com/example/app/MarketData/controller/CryptoController.java
@@ -1,25 +1,25 @@
-package com.example.app.MarketData.controller;
-
-
-import com.example.app.MarketData.model.CryptoPrice;
-import com.example.app.MarketData.service.CryptoService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-
-@RestController
-@RequestMapping("/api/crypto")
-public class CryptoController {
-    private final CryptoService cryptoService;
-
-    public CryptoController(CryptoService cryptoService) {
-        this.cryptoService = cryptoService;
-    }
-
-    @GetMapping("/average")
-    public List<CryptoPrice> getAverageCryptoPrices() {
-        return cryptoService.getAverageCryptoPrices();
-    }
-}
+//package com.example.app.MarketData.controller;
+//
+//
+//import com.example.app.MarketData.model.CryptoPrice;
+//import com.example.app.MarketData.service.CryptoService;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.util.List;
+//
+//@RestController
+//@RequestMapping("/api/crypto")
+//public class CryptoController {
+//    private final CryptoService cryptoService;
+//
+//    public CryptoController(CryptoService cryptoService) {
+//        this.cryptoService = cryptoService;
+//    }
+//
+//    @GetMapping("/average")
+//    public List<CryptoPrice> getAverageCryptoPrices() {
+//        return cryptoService.getAverageCryptoPrices();
+//    }
+//}
