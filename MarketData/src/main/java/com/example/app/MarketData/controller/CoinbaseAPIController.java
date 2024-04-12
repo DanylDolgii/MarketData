163dc6c23214bb7ef34649072464cb5745d595d4
DanylDolgii <danyldolgii777@gmail.com>
2024-04-12 18:01:42 +0300

    Changed the path

--- MarketData/src/main/java/com/example/app/MarketData/controller/CoinbaseAPIController.java
+++ MarketData/src/main/java/com/example/app/MarketData/controller/CoinbaseAPIController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/crypto")
+@RequestMapping("api/crypto")
 public class CoinbaseAPIController {
 
     private final CoinbaseAPIService coinbaseAPIService;
