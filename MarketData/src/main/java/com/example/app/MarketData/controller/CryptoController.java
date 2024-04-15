91521cb26516ff075a2dbe93edb90a34f7ded390
DanylDolgii <danyldolgii777@gmail.com>
2024-04-15 16:52:32 +0300

    Modify class, add endpoint methods and path variables

--- MarketData/src/main/java/com/example/app/MarketData/controller/CryptoController.java
+++ MarketData/src/main/java/com/example/app/MarketData/controller/CryptoController.java
@@ -1,25 +1,46 @@
-//package com.example.app.MarketData.controller;
-//
-//
-//import com.example.app.MarketData.model.CryptoPrice;
-//import com.example.app.MarketData.service.CryptoService;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import java.util.List;
-//
-//@RestController
-//@RequestMapping("/api/crypto")
-//public class CryptoController {
-//    private final CryptoService cryptoService;
-//
-//    public CryptoController(CryptoService cryptoService) {
-//        this.cryptoService = cryptoService;
-//    }
-//
-//    @GetMapping("/average")
-//    public List<CryptoPrice> getAverageCryptoPrices() {
-//        return cryptoService.getAverageCryptoPrices();
-//    }
-//}
+package com.example.app.MarketData.controller;
+
+import com.example.app.MarketData.model.CryptoPrice;
+import com.example.app.MarketData.service.CryptoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/crypto")
+public class CryptoController {
+
+    private final CryptoService cryptoService;
+
+    @Autowired
+    public CryptoController(CryptoService cryptoService) {
+        this.cryptoService = cryptoService;
+    }
+
+    @GetMapping("/prices")
+    public List<CryptoPrice> getAllCryptoPrices() {
+        return cryptoService.getAllCryptoPrices();
+    }
+
+    @GetMapping("/prices/{id}")
+    public CryptoPrice getCryptoPriceById(@PathVariable Long id) {
+        return cryptoService.getCryptoPriceById(id);
+    }
+
+    @PostMapping("/prices")
+    public CryptoPrice createCryptoPrice(@RequestBody CryptoPrice cryptoPrice) {
+        return cryptoService.saveCryptoPrice(cryptoPrice);
+    }
+
+    @PutMapping("/prices/{id}")
+    public CryptoPrice updateCryptoPrice(@PathVariable Long id, @RequestBody CryptoPrice cryptoPrice) {
+        cryptoPrice.setId(id);
+        return cryptoService.saveCryptoPrice(cryptoPrice);
+    }
+
+    @DeleteMapping("/prices/{id}")
+    public void deleteCryptoPrice(@PathVariable Long id) {
+        cryptoService.deleteCryptoPriceById(id);
+    }
+}
